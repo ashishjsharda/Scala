33f99cf7c41dad766232214e8077982ee4db400a
Ashish S <ashishjsharda@gmail.com>
2020-02-20 04:35:11 -0500

    Create addnum.scala

---
+++ addnum.scala
@@ -0,0 +1,21 @@
+/**
+ * Add 2 numbers
+ * @author asharda
+ */
+
+object scala2 {
+
+  /**
+   * Add 2 numbers
+   * @param a
+   * @param b
+   * @return int
+   */
+  def add(a:Int,b:Int): Int ={
+    return a+b;
+  }
+  def main(args: Array[String]): Unit = {
+    println("Value of addition is "+add(10,20));
+  }
+
+}
