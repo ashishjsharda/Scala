cee70da4d2c80ec215ac484d6aa5ae436a262d1c
Ashish S <ashishjsharda@gmail.com>
2020-04-24 08:14:42 -0400

    Create foreach.scala

---
+++ foreach.scala
@@ -0,0 +1,12 @@
+object foreach {
+  def main(args: Array[String]): Unit = {
+    var i=0
+    var str="abcefhklieysvnmwq"
+    for(i<-0 until(str.length))
+      {
+        println(str(i))
+      }
+  }
+
+
+}
