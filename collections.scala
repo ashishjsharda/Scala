146b660d51d3d4e9cb773caad91b555fe348b3bf
Ashish S <ashishjsharda@gmail.com>
2020-04-28 07:50:06 -0400

    Create collections.scala

---
+++ collections.scala
@@ -0,0 +1,10 @@
+object Collections {
+  def main(args: Array[String]): Unit = {
+
+   val employees=Map("rock"->1,"nick"->2,"peter"->3)
+    println("Is Map empty " + employees.isEmpty)
+    println("List of all the keys are  "+ employees.keys)
+    println("List of all the values are  " +employees.values)
+  }
+
+}
