2773c53c3fc48d69fe561316d1db77a83aaee580
Ashish S <ashishjsharda@gmail.com>
2020-06-29 16:29:29 -0400

    Create DataTypes.scala

---
+++ DataTypes.scala
@@ -0,0 +1,8 @@
+object DataTypes {
+  def main(args: Array[String]): Unit = {
+    var myvar=10
+    var myVal="Hello Scala"
+    println("Value of myvar is " +myvar +"   Value of myVal is  " +myVal)
+  }
+
+}
