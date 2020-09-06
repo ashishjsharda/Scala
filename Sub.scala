9505b810a60241f9e65cd90df6a2e2b27177b2ce
Ashish S <ashishjsharda@gmail.com>
2020-09-06 08:18:02 -0400

    Create Sub.scala

---
+++ Sub.scala
@@ -0,0 +1,18 @@
+object Sub {
+
+  def substract(a:Int,b:Int):Int={
+    return a-b
+  }
+
+  def cosine(a:Double):Double={
+    return Math.cos(a)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    println(substract(30,10))
+    println(cosine(10))
+
+  }
+
+}
