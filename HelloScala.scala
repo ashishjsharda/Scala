0cd9f44b9640f3af4f9bd7037febe12355ede638
Ashish S <ashishjsharda@gmail.com>
2020-11-28 22:53:24 -0500

    Create HelloScala.scala

---
+++ HelloScala.scala
@@ -0,0 +1,14 @@
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+    var myVar:Int=10
+    var myVal:Float=20
+    println(myVal)
+    println(myVar)
+    1.to(10)
+    var num=1+10
+    println(num)
+
+  }
+
+}
